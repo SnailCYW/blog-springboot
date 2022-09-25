1c48668144ed613f2dd2275d54826d589659c6db
SnailCYW <15218988968@163.com>
2022-09-25 21:06:41 +0800

    初始化

---
+++ src/main/java/com/wcy/blog/BlogApplication.java
@@ -0,0 +1,20 @@
+package com.wcy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Snail
+ * @version 1.0
+ * @date 2022/9/25 21:03
+ */
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+
+}
